a5f39e84fd66e8cd8073800010e2a7c8f9ac2b20
Auburus <jordi.nopa@gmail.com>
2016-12-06 10:40:12 +0100

    Completed day6

---
+++ 2016/day6.hs
@@ -0,0 +1,26 @@
+module Main where
+
+import Data.List
+import System.IO
+
+mostCommon :: String -> Char
+mostCommon = fst . head . sortBy sorted . ocurrences
+    where
+        sorted (_, val1) (_, val2) = compare val2 val1
+
+ocurrences :: String -> [(Char, Int)]
+ocurrences [] = []
+ocurrences s@(x:xs) = (x, length $ filter (==x) s) : ocurrences xs
+
+wordsToCols :: [String] -> [String]
+wordsToCols [] = []
+wordsToCols strings@(x:xs) 
+    | null x = []
+    | otherwise = (map head strings) : wordsToCols (map tail strings)
+
+main = do
+    contents <- readFile "input6.txt"
+    let contents' = "abc\nbca\nbca"
+        words = lines contents
+
+    print $ map mostCommon (wordsToCols words)
