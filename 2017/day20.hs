b2ba26afac2b2333f6b3d4c019dba4744f21d638
Jordi <jordi.nopa@gmail.com>
2018-01-30 18:00:25 -0700

    Completed day 20

--- 2017/day20.hs
+++ 2017/day20.hs
@@ -10,6 +10,8 @@ type Pos = Vec
 type Speed = Vec
 type Acc = Vec
 type Particle = (Int, (Pos, Speed, Acc))
+type Time = Int
+type Colision = (Vec, Time)
 
 main = do
     contents <- readFile "input20.txt"
@@ -17,6 +19,7 @@ main = do
     let input = zip [0,1..] . map parseInput . lines $ contents
 
     print . take 10 . closestLongTerm $ input
+    print . length . simulateWithCollisions 10000 $ input
 
 parseInput :: String -> (Pos, Speed, Acc)
 parseInput s = (takeCoords pos, takeCoords spd, takeCoords acc)
@@ -50,6 +53,13 @@ minimumSpd xs =
     in
         filter(\(i, _) -> i `elem` minSpd) xs
 
+simulateWithCollisions :: Int -> [Particle] -> [Particle]
+simulateWithCollisions 0 xs = xs
+simulateWithCollisions i xs = simulateWithCollisions (i-1)
+                            . removeCollisions
+                            . nextTick 
+                            $ xs
+
 nextTick :: [Particle] -> [Particle]
 nextTick = map update
 
@@ -58,6 +68,14 @@ update (i, ((px, py, pz), (vx, vy, vz), (ax, ay, az))) =
     (i, ((px + vx', py + vy', pz + vz'), (vx', vy', vz'), (ax, ay, az)))
     where
         (vx', vy', vz') = (vx + ax, vy + ay, vz + az)
+
+removeCollisions :: [Particle] -> [Particle]
+removeCollisions [] = []
+removeCollisions (x@(_, (x_pos, _, _)):xs)
+    | null collided = x : removeCollisions xs
+    | otherwise = removeCollisions remaining
+    where
+        (collided, remaining) = partition (\(_, (pos, _, _)) -> pos == x_pos) xs
 
 
 distance :: Particle -> Integer
@@ -72,3 +90,22 @@ spdModule (_, (_, spd, _)) = getModule spd
 getModule :: Vec -> Integer
 getModule (x, y, z) = abs x + abs y + abs z
 
+
+colisionTime :: Integral a => a -> a -> a -> Maybe Time
+colisionTime a b c
+    | b'*b' < 4 * a' * c' = Nothing
+    | b'*b' == 4 * a' * c' = 
+        case isIntegral (-b' / (2 * a')) of
+            True -> Just (round (-b' / (2*a')))
+            False -> Nothing
+    | otherwise = Nothing
+
+    where
+        a' = fromIntegral a
+        b' = fromIntegral b
+        c' = fromIntegral c
+        sol1 = (-b' + sqrt (b'*b' - 4*a'*c')) / (2 * a')
+        sol2 = (-b' - sqrt (b'*b' - 4*a'*c')) / (2 * a')
+
+isIntegral :: RealFrac a => a -> Bool
+isIntegral x = floor x == ceiling x
